4a146cbf9c4fd1fd3d34c788f2bd1632ad028b85
Runalipokale <pokalerunali52@gmail.com>
2024-06-24 20:36:41 +0530

    update order module functionality

--- models/user.js
+++ models/user.js
@@ -110,7 +110,16 @@ class User{
 
    addOrder(){
      const db = getDb()
-     return db.collection('orders').insertOne(this.cart)
+     return this.getCart().then(products=>{
+        const order={
+         items:products,
+         user:{
+            _id:new mongodb.ObjectId(this._id),
+            name: this.name
+         }
+          }
+          return db.collection('orders').insertOne(order)
+   })
      .then(result=>{
        this.cart= {items:[]};
        return db 
@@ -120,6 +129,12 @@ class User{
          {$set:{cart:{items:[]}}}
        );
      })
+   }
+
+   getOrders(){
+      const db = getDb();
+      return db.collection('orders').find({'user._id':new mongodb.ObjectId(this._id)})
+      .toArray()
    }
 
    static findById(userId){
