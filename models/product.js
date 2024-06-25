9f9b94a70325b27c4d87aaf79bda3178f365ac99
Runalipokale <pokalerunali52@gmail.com>
2024-06-25 10:03:50 +0530

    updated package.json

--- models/product.js
+++ models/product.js
@@ -1,3 +1,9 @@
+const mongoose = require('mongoose')
+
+const Schema  = mongoose.Schema;
+
+const productSchema = new Schema();
+// code using mongoDb 
 // const mongodb = require('mongodb');
 // const getDb =require('../utils/database').getDb;
 
